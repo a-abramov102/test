5795d2e3c21705c80b265ae55449e22ca76f55b9
a.abramov <artur_desu@mail.ru>
2021-06-29 16:16:12 +0500

    player added

--- index.php
+++ index.php
@@ -18,6 +18,15 @@
         <input type="url" name="url5">
         <input type="submit" value="отправить">
     </form>
+	<div>
+    <video id="myvideo" width="480" height="270" poster="poster.gif" >
+        <source src="trailer_480p.mp4" type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' />
+        <source src="trailer_480p.webm" type='video/webm; codecs="vorbis,vp8"'/> 
+    </video>
+	</div>
+	<div id="controls">
+		<span id="playpause" class="paused" >Play</span>
+	</div>
     <script
   src="https://code.jquery.com/jquery-3.6.0.js"
   integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
